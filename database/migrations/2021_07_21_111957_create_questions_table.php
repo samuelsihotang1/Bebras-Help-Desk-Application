9954fd7fd591f28cc0d3b4a7d93710976689d6ef
Samuel Sihotang <iss21041@students.del.ac.id>
2023-07-28 16:33:11 +0700

    Add pin_answer on question

--- database/migrations/2021_07_21_111957_create_questions_table.php
+++ database/migrations/2021_07_21_111957_create_questions_table.php
@@ -6,31 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateQuestionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('questions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->string('title',120)->unique();
-            $table->string('title_slug');
-            $table->string('status')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('questions', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('user_id')->constrained();
+      $table->string('title', 120)->unique();
+      $table->string('title_slug');
+      $table->string('status')->nullable();
+      $table->string('pin_answer')->nullable();
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('questions');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('questions');
+  }
 }
