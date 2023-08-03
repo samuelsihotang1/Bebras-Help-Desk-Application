b9102876e0287cea6c6f21cbc8ce09af99c9fc48
Samuel Sihotang <iss21041@students.del.ac.id>
2023-08-03 09:02:41 +0700

    Remove redundant space

--- app/Http/Controllers/User/TopicController.php
+++ app/Http/Controllers/User/TopicController.php
@@ -11,31 +11,33 @@ use App\Http\Controllers\Controller;
 
 class TopicController extends Controller
 {
-    public function store(Request $request){
+  public function store(Request $request)
+  {
 
-        $request->validate([
-            'topic_name' => 'required|alpha|unique:topics,name|max:20|min:3'
-        ]);
+    $request->validate([
+      'topic_name' => 'required|alpha|unique:topics,name|max:20|min:3'
+    ]);
 
-        $topic_name = strtolower($request->topic_name);
-        $name = ucfirst($topic_name);
+    $topic_name = strtolower($request->topic_name);
+    $name = ucfirst($topic_name);
 
-        Topic::create([
-            'name' => $name,
-            'name_slug' => Str::of($name)->slug('-')
-        ]);
+    Topic::create([
+      'name' => $name,
+      'name_slug' => Str::of($name)->slug('-')
+    ]);
 
-        return back()->with('message',['text' => 'Topic added successfully', 'class' => 'success']);
-    }
+    return back()->with('message', ['text' => 'Topic added successfully', 'class' => 'success']);
+  }
 
-    public function show(Topic $topic){
-        $topic_id = $topic->id;
-        $answers = Answer::whereHas('question',function($query)use($topic_id){
-            $query->whereHas('topics',function($q)use($topic_id){
-                $q->where('topic_id',$topic_id);
-            });
-        })->latest()->get();
-        
-        return view('user.topic.show',compact('answers','topic'));
-    }
+  public function show(Topic $topic)
+  {
+    $topic_id = $topic->id;
+    $answers = Answer::whereHas('question', function ($query) use ($topic_id) {
+      $query->whereHas('topics', function ($q) use ($topic_id) {
+        $q->where('topic_id', $topic_id);
+      });
+    })->latest()->get();
+
+    return view('user.topic.show', compact('answers', 'topic'));
+  }
 }
