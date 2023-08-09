9a1f43a3fd1d02274bbebaccdfeb660b4f23763d
Samuel Sihotang <iss21041@students.del.ac.id>
2023-08-09 14:31:41 +0700

    Delete redundant space

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,36 +12,35 @@ class HomeController extends Controller
 {
 
 
-    public function index(Request $request)
-    {
-        //jika user belum login
-        if (!auth()->check()) {
-            $answers = Answer::with(['user', 'question'])->latest()->get();
-            return view('home', compact('answers'));
-        }
-
-
-        $answers = Answer::with(['user', 'question'])->where('user_id', '!=', auth()->id())
-            ->whereNull('status')->orWhere('status', 'viewed_by_admin')->orWhere('status', 'updated_by_user')
-            ->latest()->get();
-        return view('home', compact('answers'));
+  public function index(Request $request)
+  {
+    //jika user belum login
+    if (!auth()->check()) {
+      $answers = Answer::with(['user', 'question'])->latest()->get();
+      return view('home', compact('answers'));
     }
 
 
-    //api search
+    $answers = Answer::with(['user', 'question'])->where('user_id', '!=', auth()->id())
+      ->whereNull('status')->orWhere('status', 'viewed_by_admin')->orWhere('status', 'updated_by_user')
+      ->latest()->get();
+    return view('home', compact('answers'));
+  }
+
+
+  //api search
 
 
 
-    public function search(Request $request)
-    {
-        $question = [];
+  public function search(Request $request)
+  {
+    $question = [];
 
-        if ($request->has('q')) {
-            $search = $request->q;
-            $question = Question::select('title', 'title_slug')->where('title', 'LIKE', "%$search%")->get();
-        }
-
-        return response()->json($question);
+    if ($request->has('q')) {
+      $search = $request->q;
+      $question = Question::select('title', 'title_slug')->where('title', 'LIKE', "%$search%")->get();
     }
 
+    return response()->json($question);
+  }
 }
