7c62212c168e173e9815c9a3a18afb116fd251e9
LAPTOP-L6GF21A1\Lenovo <89496601+SamuelSiagian03@users.noreply.github.com>
2023-08-09 11:57:34 +0700

    update bahasa login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -153,7 +153,7 @@
                     <div>
                         <button type="submit"
                             class="italic flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                            Sign in
+                            Masuk
                         </button>
 
                     </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -88,8 +88,7 @@
             src="https://github.com/Estomihi100103/HelpDesk-Applicaion/assets/89466828/e1c6b6d5-33a6-4f84-86f0-058f812c3e32"
             alt="Your Company">
         <h2 class="mt-3 mb-4 text-center text-xl font-bold italic leading-9 tracking-tight text-gray-900">
-            Registration in to
-            Help Desk Application Bebras
+            Pendaftaran ke Aplikasi Help Desk Bebras
         </h2>
 
 
@@ -118,7 +117,7 @@
 
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900 italic">Email
-                        address</label>
+                        </label>
                     <div class="mt-2">
                         <input id="email" name="email" type="email" required autocomplete="email" autofocus
                             class="form-control block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 @error('email') is-invalid @enderror"
@@ -135,7 +134,7 @@
 
                 <div>
                     <label for="password"
-                        class="block text-sm font-medium leading-6 text-gray-900 italic">Password</label>
+                        class="block text-sm font-medium leading-6 text-gray-900 italic">Kata Sandi</label>
                     <div class="mt-2">
                         <input id="password" name="password" type="password" required autocomplete="current-password"
                             class="form-control block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 @error('password') is-invalid @enderror ">
@@ -151,8 +150,8 @@
 
                 <div>
                     <label for="password_confirmation"
-                        class="block text-sm font-medium leading-6 text-gray-900 italic">Confirm
-                        Password</label>
+                        class="block text-sm font-medium leading-6 text-gray-900 italic">Konfirmasi
+                        Kata Sandi</label>
                     <div class="mt-2">
                         <input id="password_confirmation" name="password_confirmation" type="password" required
                             autocomplete="current-password"
@@ -163,17 +162,16 @@
                 <div>
                     <button type="submit"
                         class="italic flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                        Registration
+                        Daftar
                     </button>
 
                 </div>
             </form>
             <p class="mt-10 text-center text-sm text-gray-500">
-                Already have account?
+                Sudah memiliki akun?
                 <!-- space -->
                 <a href="{{ route('login') }}"
-                    class="font-semibold leading-6 text-blue-600 hover:text-indigo-500 italic">Sign
-                    in</a>
+                    class="font-semibold leading-6 text-blue-600 hover:text-indigo-500 italic">Masuk</a>
             </p>
 
             <div>
