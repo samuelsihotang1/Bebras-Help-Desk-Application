2ae67e2f4c0a3cc9f3fa2ac9a4582a7a70138d0e
Samuel Sihotang <iss21041@students.del.ac.id>
2023-08-09 12:04:54 +0700

    Delete redundant space

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,120 +2,120 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="Bebras">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="description" content="Bebras">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Bebras')</title>
+  <title>@yield('title', 'Bebras')</title>
 
-    <!-- Icon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
-    <link rel="manifest" href="/icon/site.webmanifest">
+  <!-- Icon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
+  <link rel="manifest" href="/icon/site.webmanifest">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="{{ asset('js/share.js') }}"></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <script src="{{ asset('js/share.js') }}"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+  <!-- Styles -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
-    @yield('link')
-    <style>
-        .dmenu a {
-            width: 250px;
-        }
-    </style>
-    @livewireStyles
+  @yield('link')
+  <style>
+    .dmenu a {
+      width: 250px;
+    }
+  </style>
+  @livewireStyles
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white border-bottom sticky-top">
-            <div class="container">
-                <a href="{{ route('home') }}">
-                    <img class="mx-auto d-block mb-0"
-                    src="https://github.com/Estomihi100103/HelpDesk-Applicaion/assets/89466828/e1c6b6d5-33a6-4f84-86f0-058f812c3e32"
-                    alt="Your Company" height="50"></a>
-                
-                <a class="navbar-brand mr-5" href="{{ route('home') }}">
-                    <b class="px-3 text-danger" style="font-weight: bold;">Bebras Help Desk</b>
-                </a>
+  <div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white border-bottom sticky-top">
+      <div class="container">
+        <a href="{{ route('home') }}">
+          <img class="mx-auto d-block mb-0"
+            src="https://github.com/Estomihi100103/HelpDesk-Applicaion/assets/89466828/e1c6b6d5-33a6-4f84-86f0-058f812c3e32"
+            alt="Your Company" height="50"></a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <a class="navbar-brand mr-5" href="{{ route('home') }}">
+          <b class="px-3 text-danger" style="font-weight: bold;">Bebras Help Desk</b>
+        </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        @guest
-                        @else
-                            <li class="nav-item ">
-                                <a href="{{ route('home') }}"
-                                    class="{{ request()->route()->named('home')? 'text-danger': 'text-dark' }} "><i
-                                        class="bi bi-house-door" style="font-size: 1.5rem;"></i></a>
-                            </li>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-                            @can('isAdmin')
-                                <x-admin-answers />
-                                <x-admin-questions />
-                                <x-admin-comments />
-                                <x-admin-topics />
-                                <x-admin-users />
-                                <x-admin-faqs />
-                            @else
-                                <li class="nav-item ml-4 mr-4">
-                                    <a href="{{ route('answer.index') }}"
-                                        class="{{ request()->route()->named('answer.index') ||request()->route()->named('question.show')? 'text-danger': 'text-dark' }}"><i
-                                            class="bi bi-pencil-square" style="font-size: 1.5rem;"></i></a>
-                                </li>
-                                <li class="nav-item ml-4 mt-1">
-                                    <select name="livesearch" class="form-control livesearch" style="width: 450px;"></select>
-                                </li>
-                            @endcan
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
+            @guest
+            @else
+            <li class="nav-item ">
+              <a href="{{ route('home') }}"
+                class="{{ request()->route()->named('home')? 'text-danger': 'text-dark' }} "><i class="bi bi-house-door"
+                  style="font-size: 1.5rem;"></i></a>
+            </li>
 
-                        @endguest
+            @can('isAdmin')
+            <x-admin-answers />
+            <x-admin-questions />
+            <x-admin-comments />
+            <x-admin-topics />
+            <x-admin-users />
+            <x-admin-faqs />
+            @else
+            <li class="nav-item ml-4 mr-4">
+              <a href="{{ route('answer.index') }}"
+                class="{{ request()->route()->named('answer.index') ||request()->route()->named('question.show')? 'text-danger': 'text-dark' }}"><i
+                  class="bi bi-pencil-square" style="font-size: 1.5rem;"></i></a>
+            </li>
+            <li class="nav-item ml-4 mt-1">
+              <select name="livesearch" class="form-control livesearch" style="width: 450px;"></select>
+            </li>
+            @endcan
 
-                    </ul>
+            @endguest
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+          </ul>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown px-4">
-                                <a id="navbarDropdown" class="nav-link ml-4" href="javascript: void(0)" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <img src="{{ strpos(Auth::user()->avatar, 'https') === 0 ? Auth::user()->avatar : asset('img/' . Auth::user()->avatar) }}"
-                                        alt="avatar" class="rounded-circle" width="35px" height="35px">
-                                </a>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+            @if (Route::has('login'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @endif
+
+            @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+            @else
+            <li class="nav-item dropdown px-4">
+              <a id="navbarDropdown" class="nav-link ml-4" href="javascript: void(0)" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <img
+                  src="{{ strpos(Auth::user()->avatar, 'https') === 0 ? Auth::user()->avatar : asset('img/' . Auth::user()->avatar) }}"
+                  alt="avatar" class="rounded-circle" width="35px" height="35px">
+              </a>
 
               <div class="dropdown-menu dropdown-menu-right dmenu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="{{ route('profile.index', Auth::user()->name_slug) }}" class="text-dark">
@@ -143,30 +143,30 @@
               </div>
             </li>
 
-                            <button class="btn btn-sm btn-danger mt-2 " data-toggle="modal"
-                                data-target="#add-questionModal" style="height: 36px; width:140px">Tambah Pertanyaan</button>
+            <button class="btn btn-sm btn-danger mt-2 " data-toggle="modal" data-target="#add-questionModal"
+              style="height: 36px; width:140px">Tambah Pertanyaan</button>
 
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-        <main class="py-4">
-            @guest
-            @else
-                <x-question />
             @endguest
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-            @yield('content')
-        </main>
-    </div>
+
+    <main class="py-4">
+      @guest
+      @else
+      <x-question />
+      @endguest
+
+      @yield('content')
+    </main>
+  </div>
 
 
 
-    <script>
-        let $q = $('.livesearch');
+  <script>
+    let $q = $('.livesearch');
 
         $q.select2({
             placeholder: 'Search question',
@@ -209,11 +209,11 @@
                 window.location.href = "/" + e.params.data.id
             }
         });
-    </script>
+  </script>
 
 
-    @yield('script')
-    @livewireScripts
+  @yield('script')
+  @livewireScripts
 
 </body>
 
