4df29448972b201d1412c06e87aaaa4b95fd9ecf
Samuel Sihotang <iss21041@students.del.ac.id>
2023-09-16 22:35:36 +0700

    fixx bug on create question

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -31,7 +31,7 @@
             
                     <div class="row mt-3">
                         <div class="col-sm-12">
-                            <input type="text" name="title" class="form-control" placeholder="Berikan pertanyaan" autocomplete="off" id="q-title">
+                            <input type="text" name="title" class="form-control" placeholder="Berikan pertanyaan" autocomplete="off" id="q-title" required>
                             @include('layouts.error', ['name' => 'title'])
                         </div>
                     </div>
