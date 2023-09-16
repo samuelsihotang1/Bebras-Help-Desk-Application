81c41d193099f11cdde98ba7040f15971b54893e
Samuel Sihotang <iss21041@students.del.ac.id>
2023-09-16 22:35:14 +0700

    Modify popular topic being sticky

--- resources/views/components/popular-topic.blade.php
+++ resources/views/components/popular-topic.blade.php
@@ -1,39 +1,39 @@
+<div class="card" style="position: sticky; top: 120px">
+  <div class="card-header">
+    Topik
+    @can('isAdmin')
+    <button class="btn btn-sm btn-outline-primary float-right" id="btnTopic">Tambah Topik</button>
+    @endcan
+  </div>
+  <div class="card-body">
+    <div id="formTopic">
+      <form action="{{ route('create.topic') }}" method="POST">
+        @csrf
+        <div class="input-group input-group-sm">
+          <input type="text" name="topic_name" class="form-control @error('topic_name') is-invalid @enderror"
+            autocomplete="off">
 
-<div class="card">
-    <div class="card-header">
-        Topik Populer
-        <button class="btn btn-sm btn-outline-primary float-right" id="btnTopic">Tambah Topik</button>
-    </div>
-    <div class="card-body">
-        <div id="formTopic">
-            <form action="{{ route('create.topic') }}" method="POST">
-                @csrf
-                <div class="input-group input-group-sm">
-                    <input type="text" name="topic_name" class="form-control @error('topic_name') is-invalid @enderror" autocomplete="off">
-                    
-                    @error('topic_name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    
-                </div>
+          @error('topic_name')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
 
-                <button type="submit" class="btn btn-sm btn-primary mt-2 rounded-pill">Tambah</button>
-            </form>
-            <hr>
         </div>
-        @foreach($topics as $topic)
-            <a href="{{ route('topic.show',$topic->name_slug) }}" class="text-dark">{{ $topic->name }} 
-                <div class="btn btn-outline-secondary float-right btn-sm rounded-pill">
-                {{ $topic->follower }} Pengikut</div></a>
-                @if ($loop->last)
-                @else
-                <hr>
-                @endif
-        @endforeach
+
+        <button type="submit" class="btn btn-sm btn-primary mt-2 rounded-pill">Tambah</button>
+      </form>
+      <hr>
     </div>
+    @foreach($topics as $topic)
+    <a href="{{ route('topic.show',$topic->name_slug) }}" class="text-dark">{{ $topic->name }}
+      <div class="btn btn-outline-secondary float-right btn-sm rounded-pill">
+        {{ $topic->follower }} Pengikut</div>
+    </a>
+    @if ($loop->last)
+    @else
+    <hr>
+    @endif
+    @endforeach
+  </div>
 </div>
-
-
-
