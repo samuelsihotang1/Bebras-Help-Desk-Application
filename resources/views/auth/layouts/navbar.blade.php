5b53194f6cbc5d16a5075dce0a483469cc03925a
Samuel Sihotang <iss21041@students.del.ac.id>
2023-09-16 17:15:39 +0700

    Add icon

--- resources/views/auth/layouts/navbar.blade.php
+++ resources/views/auth/layouts/navbar.blade.php
@@ -2,23 +2,30 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="/css/components.css">
-    <script src="/js/components.js"></script>
-    <script type="module" src="/js/iframe-alpine-964dceff.js"></script>
-    <title>@yield('title', 'Bebras')</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="/css/components.css">
+  <script src="/js/components.js"></script>
+  <script type="module" src="/js/iframe-alpine-964dceff.js"></script>
+
+  <!-- Icon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
+  <link rel="manifest" href="/icon/site.webmanifest">
+
+  <title>@yield('title', 'Bebras')</title>
 </head>
 
 <body>
-    <div class="" style="">
-        @yield('content')
+  <div class="" style="">
+    @yield('content')
 
-    </div>
+  </div>
 
 
-    </div>
+  </div>
 </body>
 
 </html>
