faa87e2bfe14396124dda513e52a50bbada3fded
Samuel Sihotang <iss21041@students.del.ac.id>
2023-09-16 17:23:17 +0700

    fixing bugs on search question

--- resources/views/livewire/search-question.blade.php
+++ resources/views/livewire/search-question.blade.php
@@ -2,7 +2,7 @@
   <div>
     <form action="/search" method="GET">
       <input placeholder="Cari Pertanyaan..." @click="open = true" @click.away="open = false" name="search" type="text"
-        class="form-control" wire:model="search">
+        class="form-control" wire:model="search" required>
     </form>
     <div x-show="open"
       style="padding-left: 0.75rem !important;width: 450px; position: absolute; z-index: 10; max-height: 15rem; overflow: auto; border-radius: 0.375rem; background-color: #FFFFFF; font-size: 1rem; box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 3px 0 rgba(0, 0, 0, 0.1); outline: 1px solid #00000010;">
